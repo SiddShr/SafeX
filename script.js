4be86acefec7bba51b1ab943be6a78b641668489
Kimng Gnmik <kimng@Kimngs-MacBook-Pro.local>
2022-03-22 23:41:00 -0400

    New Darkmode update with slides

--- script.js
+++ script.js
@@ -25,3 +25,12 @@ window.addEventListener('DOMContentLoaded', ()=>{
         }, 3500)
     })
 })
+
+$(document).ready(function() {
+    $('.template-article img').each(function() {
+        var currentImage = $(this);
+        currentImage.wrap("<a class='image-link' href='" + currentImage.attr("src") + "'</a>");
+    });
+    $('.image-link').magnificPopup({type:'image'});  
+  });
+    
